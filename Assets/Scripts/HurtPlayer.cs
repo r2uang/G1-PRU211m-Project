f80142bddb8a6f9e3c417f93f9ff7ed010890fc9
dinhhiep110 <dinhhiep110@gmail.com>
2022-10-18 09:04:04 +0700

    Use Trigger For Hurt Player

--- Assets/Scripts/HurtPlayer.cs
+++ Assets/Scripts/HurtPlayer.cs
@@ -18,9 +18,9 @@ public class HurtPlayer : MonoBehaviour
     {
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
             healthManager.HurtPlayer(enemy.enemyData.meleeDamage);
         }
